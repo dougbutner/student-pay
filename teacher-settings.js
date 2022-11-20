505e8bd3b81cd49c4453c96232e10824e364cf15
Douglas Butner <dougbutner@gmail.com>
2022-11-19 22:44:36 -0600

    Added Teacher Settings

---
+++ teacher-settings.js
@@ -0,0 +1,26 @@
+// === TEACHER SETTINGS === \\
+
+existing_studentlist = false;
+
+// --- Unit per correct answer --- \\
+const correct_pay = 1;
+
+// --- Unit per incorrect answer --- \\
+const incorrect_pay = 0;
+
+// --- Unit per quiz started --- \\
+const start_pay = 0;
+
+// --- Unit per quiz finished --- \\
+const finish_pay = 0;
+
+
+
+/*/ Static Student List
+
+const students = [ // "wordpress_account_name":"wallet.near"
+  "douglas":"goudasol.near",
+  "sonny":"sonnylife.near"
+];
+
+/*/
