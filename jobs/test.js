d5d21250cd253386ea67eae0ce6437a7ff46e55d
Douglas Butner <dougbutner@gmail.com>
2022-11-19 22:33:43 -0600

    Bree job set up with start of Job template

---
+++ jobs/test.js
@@ -0,0 +1,129 @@
+
+const { parentPort } = require('worker_threads');
+
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');      // development only
+const fetch = require('node-fetch');                                    // node only; not needed in browsers
+const { TextEncoder, TextDecoder } = require('util');                   // node only; native TextEncoder/Decoder
+
+const defaultPrivateKey = process.env.CXC_ASSETS; // thx4dapurple blux Main net
+const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
+const rpc = new JsonRpc('https://wax.greymass.com', { fetch });
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+// --- Custom Classes --- \\
+const Bagman = require('../bagman.js');
+const Chipper = require('../chipper.js');
+
+// --- Security --- \\
+require('dotenv').config();
+
+// --- Get API --- \\
+const https = require('https');
+const axios = require('axios').default;
+
+
+
+
+// --- Timmy Varibles --- \\
+peeps = [];
+neededCollectionNum = 6;
+templateId = 272347;
+chipCount = 1; 
+
+const promPeeps = new Promise((resolve, reject) => {
+  
+  // --- Get the Holders NFTs from the API --- \\
+  let aaurl = 'https://wax.api.atomicassets.io/atomicassets/v1/accounts';//?collection_name=cxcmusicnfts&hide_offers=true&collection_whitelist=cxcmusicnfts&page=1&limit=99&order=desc';
+  let aadata = {
+    collection_name:"cxcmusicnfts",
+    collection_whitelist:"cxcmusicnfts",
+    hide_offers:true,
+    page:1,
+    limit:100,
+    order:"asc"
+  }
+  aaurl="https://wax.api.atomicassets.io/atomicassets/v1/accounts?collection_whitelist=cxcmusicnfts&page=2&limit=100&order=asc"
+
+  axios({
+    method: 'get',
+    url: aaurl,
+    data:aadata
+  })
+    .then(function (response) {
+
+      console.log("response.data.length", response.data.data.length);
+
+      for (var i=0, n=response.data.data.length; i < n; ++i){ //Loop Through Holders
+        let holder = response.data.data[i];
+        if (parseInt(holder.assets) >= neededCollectionNum){
+          // --- Add the person to the list  --- \\
+          peeps.push(
+            [
+              holder.assets, //0
+              holder.account, //1
+            ]
+          );
+        }//END if response != undefined
+        else { // Loop is done, as results are descending
+          break;
+        } //END Else response != undefined
+        
+      }//END Holder Getter  loop        
+              resolve(peeps);
+    }).catch(function (error) {
+      // handle error
+      console.log(error);
+    });//END axios final
+
+});//END Promise promPeeps
+
+
+  
+ // --- Prepare Txs with eligible people list --- \\ 
+promPeeps.then((peeps) =>{
+
+let batch = [];
+
+
+peeps.forEach(function (item, index) {
+  
+  console.log(item[1] + " has "+ item[0] + " NFTs.");
+  
+  batch.push({
+    contract: 'atomicassets',
+    action:'mintasset',
+    actor: 'currentxchng',
+    permission:'assets',
+    data: {
+    }
+  });
+});
+
+
+//console.log("batch", batch);
+
+    
+    
+    delete len, chipCount, peeps;
+        
+    return true;
+}).catch(err => { console.log(err)});
+
+
+// === Batch Handlers === \\
+
+
+
+// === Bree Schenanigans === \\
+
+function cancel() {
+  clearTimeout(); 
+  // send a message to the parent that we're ready to terminate
+  // (you could do `process.exit(0)` or `process.exit(1)` instead if desired
+  // but this is a bit of a cleaner approach for worker termination
+  if (parentPort) parentPort.postMessage('cancelled');
+  else process.exit(0);
+
+  
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ms = require('ms');
+const dayjs = require('dayjs');
+const Bree = require('bree');
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const bree = new Bree({
+  jobs: [
+    
+    // --- Status Log for Teacher --- \\
+    {
+      name: 'api-test',
+      timeout: '0'
+    },
+    // --- Quiz Completion Drops --- \\
+    {
+      name: 'student-pay-near',
+      interval: '5m'
+    },
+    // --- Student Engagement Awards - Extra Credit --- \\
+    {
+      name: 'daily-xtr-credit',
+      interval: 'at 2:00 pm' // Everyday
+    },
+    { 
+      name: 'weekly-xtr-credit',
+      cron: '30 3 * * 7' // Mondays at 7:30 AM 
+    },
+    { 
+      name: 'monthly-xtr-credit',
+      cron: '24 7 1 * *' // At 7:24 AM on first of each month
+    },
+    { 
+      name: 'yearly-xtr-credit',
+      cron: '0 1 1 1 *' // At 01:00 AM on Jan 1
+    },
+  ]
+});
+
+
+// === Start the Rewards === \\
+bree.start();
+console.log("Student Pay Launched");
+
+bree.on('worker created', (name) => {
+  console.log('Task Started ', name);
+});
+
+bree.on('worker deleted', (name) => {
+  console.log('Task Ended ', name);
+});
+
+
+/*/
+
+app.get('/', (req, res) => {
+  res.send('Hello There')
+});
+/*/
